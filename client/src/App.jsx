08925185ef1ef931a5f3329fdf81d90e7f65b369
Sumit Kumar <developer.sumit.professional@gmail.com>
2025-10-08 10:05:05 +0530

    Fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
           path="/user-login"
           element={
             <UserPublicWrapper>
-              <ChooseRide />
+              <UserLogin />
             </UserPublicWrapper>
           }
         />
